a191447401d49d3e15c9eec5fbc4e13cdfe6e57c
lykmapipo <lallyelias87@gmail.com>
2018-10-02 18:35:22 +0300

    allow fetching bill history from multiple tables

--- index.js
+++ index.js
@@ -3,5 +3,8 @@
 
 /* dependencies */
 const path = require('path');
+const db = require(path.join(__dirname, 'lib', 'db'));
 
-exports = module.exports = require(path.join(__dirname, 'lib', 'db'));
+
+/* exports */
+exports = module.exports = db;
