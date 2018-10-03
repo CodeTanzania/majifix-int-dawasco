b8986761e8ed9368226442d22e29b48ee09ca38a
lykmapipo <lallyelias87@gmail.com>
2018-10-03 11:38:11 +0300

    drop check previous and current bills on shouldFetch conditions

--- index.js
+++ index.js
@@ -5,10 +5,6 @@
 const path = require('path');
 const db = require(path.join(__dirname, 'lib', 'db'));
 
-db.getBillHistory('A8668801', function(error, bills) {
-    console.log(error);
-    console.log(bills);
-});
 
 /* exports */
 exports = module.exports = db;
